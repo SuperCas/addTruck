e60c91b7a32af98cd9901bf33ae631c18fb0436a
Christian <Chrizzle@Christians-MBP.fritz.box>
2018-06-26 13:41:55 +0200

    Changes2

--- FirstDjangoAgain/logisticsSettings/forms.py
+++ FirstDjangoAgain/logisticsSettings/forms.py
@@ -4,5 +4,5 @@ from logisticsSettings.models import Truck
 class NameForm(forms.ModelForm):
 	class Meta:
 		model = Truck
-		fields = ['truck_name']
+		fields = ['truck_name', 'weight', 'length', 'width', 'height']
 		labels = '__all__'
